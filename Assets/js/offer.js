8cac46168bb6389e6c53d187492677bc7ede6915
Ashish Kumar Nayak <90566976+Ashish-kumar-Nayak-159@users.noreply.github.com>
2023-04-12 00:40:36 +0530

    a

--- Assets/js/offer.js
+++ Assets/js/offer.js
@@ -1,5 +1,4 @@
-
-const from = document.getElementById("from");
+        const from = document.getElementById("from");
         const to = document.getElementById("to");
         const seat = document.getElementById("seat");
         const sdate = document.getElementById("s-date");
@@ -8,42 +7,55 @@ const from = document.getElementById("from");
         const r_from = document.getElementById("r_from");
         const r_to = document.getElementById("r_to");
 
-        function display_offer() {
-            if (from.value == " ") {
-                phase2_enable.style.display = "none";
-                r_from.style.display = "flex";
-            }
-            else {
-                phase2_enable.style.display = "block";
-            }
-            if (to.value == " ") {
-                phase2_enable.style.display = "none";
-                r_to.style.display = "flex";
-            } else {
-                phase2_enable.style.display = "block";
-            }
-        }
+        # function display_offer() {
+        #     if (from.value == " ") {
+        #         phase2_enable.style.display = "none";
+        #         r_from.style.display = "flex";
+        #     }
+        #     else {
+        #         phase2_enable.style.display = "block";
+        #     }
+        #     if (to.value == " ") {
+        #         phase2_enable.style.display = "none";
+        #         r_to.style.display = "flex";
+        #     } else {
+        #         phase2_enable.style.display = "block";
+        #     }
+        # }
 
-        function hide2() {
+        function hide() {
             if (from.value != "") {
                 r_from.style.display = "none";
-                
-            } else {
+            } 
+            else {
                 r_from.style.display = "flex";
-
-            }
-        }
-        function hide1() {
-            if (to.value != "") {
-                r_to.style.display = "none";
-
-            } else {
-                r_to.style.display = "flex";
-
             }
         }
 
 
-        function phase2_none(){
-            phase2_enable.style.display = "none";
-        }
+        # function hide1() {
+        #     if (to.value != "") {
+        #         r_to.style.display = "none";
+
+        #     } else {
+        #         r_to.style.display = "flex";
+
+        #     }
+        # }
+
+
+        # function phase2_none(){
+        #     phase2_enable.style.display = "none";
+        # }
+
+
+        # function function display_offer(){
+        #     if(from.value == " "){
+        #         r_from.style.display = "flex";
+
+        #     }else
+        #     {
+        #         phase2_enable.style.display = "block";
+
+        #     }
+        # }
